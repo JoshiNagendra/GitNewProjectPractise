f2bf09339e259634c22ce968696dd2ed49592ec5
JoshiNagendra <joshichennamsetti999@gmail.com>
2023-07-29 17:36:59 +0530

    first commit

---
+++ BootMVCProj-01-DisplayingHomePage-1/src/main/java/in/joshi/BootMvcProj01DisplayingHomePage1Application.java
@@ -0,0 +1,13 @@
+package in.joshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj01DisplayingHomePage1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj01DisplayingHomePage1Application.class, args);
+	}
+
+}
---
+++ BootMVCProj-01-DisplayingHomePage-1/src/test/java/in/joshi/BootMvcProj01DisplayingHomePage1ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.joshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMvcProj01DisplayingHomePage1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
